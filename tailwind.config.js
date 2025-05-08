52375d6fa4987907c8246c908a37112bb8c0c95e
Aniket <rupareliaaniket7@gmail.com>
2025-05-08 14:22:44 +0530

    error fixed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+import scrollbarHide from 'tailwind-scrollbar-hide';
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [scrollbarHide],
+};
